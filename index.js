b5a275be26e52148700fe7bd1df0d59551e7bfcb
pontshoL <pontshol29@gmail.com>
2022-05-05 09:41:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+let name1 = 'Pontsho';
+let name2 = 'Lindiwe'
+let surname = 'Ramodipa,';
+let address = ' I come from a small village called viking around boyne.';
+let body = 'As a recent graduate, I am seeking a role which allows me to continue learning and perfecting my skills'
++' as I provide high-quality work, and encourages me to flourish.'
++'I am highly motivated and dedicated with good communication skills. Excellent time management and great listeaner'+"<br></br>";
+
+let body1 = 'I am energetic and a great communicator. Work integrated learning helped me to build my self-confidence.'
++' I am punctual, dependable and can be counted upon to finish what I start.'
++' I consider myself hardworking, reliable, organised, honest and cooperative.'+"<br></br>"
+
+let body3 = 'I completed my work intergrated learning at a company called Endeva technologies  from,'
++'September 2021 until April 2022. I was performing tasks such as administrative duties,'
++'Windows Installations, PC upgrade from HDD TO SSD, Web and mail hosting, web design with wordpress,'
++'IQ RETAIL , doing Quotes, Invoices, Job cards, ETC.'+ "<br></br>"
+
+let project = 'I have worked on a project with my colleagues . We developed a desktop application that will'
++' be used by attorneys and law firms to peform tasks such as Client management, Case management, Accounting,'
++'and ticketing. We used c# and MySql to develop it.'+ "<br></br>"
+let body2 = 'Knowledge of programming languages'
++' C++,  HTML & CSS , PLSQL, C#, MySQL'
++'<p>OTHER SKILLS AND COMPETANCIES.</p>'
++'<p>Critical thinking</p>'
++ '<p>hardworker</p>'
++'Decision Maker'
+
+let body4 = ' <h3 style="color: rgb(202, 134, 66);">'
++'<b>'
++'TSHWANE UNIVERSITY OF TECHNOLOGY'
++'</b>'
++'</h3>'
+
++'<p>'
+ +'National Diploma <br />'
+ +'Information Technology in Software Development <br />'
+ +'2018-2021 <br />'
++'</p>'
+
+
+document.getElementById('test1').innerHTML='I am '+ name1+' '+name2+' '+surname+' '+address+' '+body+' '+body1+' '+project+' '+ body2+' '+body4;
+
